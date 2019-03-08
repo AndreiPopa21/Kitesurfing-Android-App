991971ba6f44709127d1e5fbbbc7ffc6b7001b0f
Stefan Popa <andrei.popa2199@gmail.com>
2019-03-08 06:03:13 +0200

    Implemented in NetworkUtils a method for displaying api-spot-get-all call response

--- app/src/main/java/com/example/stefanpopa/kitesurfingandroidproject/MainActivity.java
+++ app/src/main/java/com/example/stefanpopa/kitesurfingandroidproject/MainActivity.java
@@ -17,7 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG="MainActivity";
+    public static final String TAG="MainActivity";
     //private static final String BASE_URL= "https://internship-2019.herokuapp.com";
 
     @Override
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Auth_Body auth = new Auth_Body(getString(R.string.valid_email));
+        //Auth_Body auth = new Auth_Body(getString(R.string.valid_email));
         Spot_All_Body spot = new Spot_All_Body(null,70);
         sendNetworkAuthRequest(spot);
     }
@@ -39,29 +39,11 @@ public class MainActivity extends AppCompatActivity {
         KitesurfingAPI kitesurfingAPI = retrofit.create(KitesurfingAPI.class);
 
         Call<Spot_All_Result> call = kitesurfingAPI.getSpotAll(spot);
-        /*call.enqueue(new Callback<Auth_Result>() {
-            @Override
-            public void onResponse(Call<Auth_Result> call, Response<Auth_Result> response) {
-                Log.d(TAG,"onResponse: Server Response is: "+response.toString());
-                Log.d(TAG,"onResponse: Server Response Token is: "+response.body().getResult_children().getToken());
-                Log.d(TAG,"onResponse: Server Response Email is: "+response.body().getResult_children().getEmail());
-                //Toast.makeText(MainActivity.this,response.body().getToken(),Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onFailure(Call<Auth_Result> call, Throwable t) {
-                Log.d(TAG,"onFailure: Something went wrong with the server");
-            }
-        });*/
         call.enqueue(new Callback<Spot_All_Result>() {
             @Override
             public void onResponse(Call<Spot_All_Result> call, Response<Spot_All_Result> response) {
-                Log.d(TAG,"onResponse: Server Response is: "+response.toString());
-                Log.d(TAG,"onResponse: Server Response ID is: "+response.body().getAll_result_children().get(1).getId());
-                Log.d(TAG,"onResponse: Server Response Name is: "+response.body().getAll_result_children().get(1).getName());
-                Log.d(TAG,"onResponse: Server Response Country is: "+response.body().getAll_result_children().get(1).getCountry());
-                Log.d(TAG,"onResponse: Server Response WhenToGo is: "+response.body().getAll_result_children().get(1).getWhenToGo());
-                Log.d(TAG,"onResponse: Server Response IsFavorite is: "+response.body().getAll_result_children().get(1).getIsFavorite());
+                NetworkUtils.displayResponseGetAllSpot(response);
             }
 
             @Override
--- app/src/main/java/com/example/stefanpopa/kitesurfingandroidproject/NetworkUtils.java
+++ app/src/main/java/com/example/stefanpopa/kitesurfingandroidproject/NetworkUtils.java
@@ -1,4 +1,29 @@
 package com.example.stefanpopa.kitesurfingandroidproject;
 
+import android.util.Log;
+
+import com.example.stefanpopa.kitesurfingandroidproject.api_spot_get_all_models.Spot_All_Result;
+import com.example.stefanpopa.kitesurfingandroidproject.api_user_get_models.Auth_Result;
+
+import retrofit2.Response;
+
 public class NetworkUtils {
+
+    public static void displayResponseGetAllSpot(Response<Spot_All_Result> response){
+        int count = response.body().getAll_result_children().size();
+        for(int i=0;i<count;i++) {
+            Log.d(MainActivity.TAG, "\nonResponse: The item " + i + " has the following details:" +
+                    "\nID is: " + response.body().getAll_result_children().get(i).getId() +
+                    "\nName is: " + response.body().getAll_result_children().get(i).getName() +
+                    "\nCountry is: " + response.body().getAll_result_children().get(i).getCountry() +
+                    "\nWhenToGo is: " + response.body().getAll_result_children().get(i).getWhenToGo() +
+                    "\nIsFavorite is: " + response.body().getAll_result_children().get(i).getIsFavorite());
+        }
+    }
+
+    public static void displayResponseUserGet(Response<Auth_Result> response){
+        Log.d(MainActivity.TAG,"onResponse:"+
+            "\nToken is: "+response.body().getResult_children().getToken()+
+            "\nEmail is: "+response.body().getResult_children().getEmail());
+    }
 }
