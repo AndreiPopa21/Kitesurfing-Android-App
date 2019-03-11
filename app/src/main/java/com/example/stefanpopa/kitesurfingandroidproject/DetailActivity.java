1f8d1fdb619a08bbeb847a15ee251a9fca579e0e
Stefan Popa <andrei.popa2199@gmail.com>
2019-03-11 22:42:20 +0200

    Created ProgressBar and TextView for DetailActivity, handled them against configuration changes

--- app/src/main/java/com/example/stefanpopa/kitesurfingandroidproject/DetailActivity.java
+++ app/src/main/java/com/example/stefanpopa/kitesurfingandroidproject/DetailActivity.java
@@ -5,21 +5,40 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String DETAIL_TAG="DetailActivity";
+    public static final String IS_DETAIL_NO_CONNECTION_TEXT_VIEW_VISIBLE="is_detail_no_connection_text_view_visible";
+    public static final String IS_DETAIL_PROGRESS_BAR_VISIBLE="is_detail_progress_bar_visible";
+    public static final String DETAIL_ON_SAVED_INSTANCE="detail_on_saved_instance";
+
+    private ProgressBar detailProgressBar;
+    private TextView detailNoConnectionTextView;
+
+    //boolean that tells us whether a configuration change took place
+    private boolean detailOnSavedInstance=false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
-        Intent intentThatStartedActivity = getIntent();
-        if(intentThatStartedActivity==null){
-            closeOnError("Passed empty Intent to the DetailActivity");
-        }
-        if(!checkIntentContent(intentThatStartedActivity)){
-            closeOnError("The extras do not follow the specified format");
+
+        bindViews();
+
+        if(savedInstanceState==null){
+            Intent intentThatStartedActivity = getIntent();
+            if(intentThatStartedActivity==null){
+                closeOnError("Passed empty Intent to the DetailActivity");
+            }
+            if(!checkIntentContent(intentThatStartedActivity)){
+                closeOnError("The extras do not follow the specified format");
+            }
+        }else {
+            checkViewsVisibility(savedInstanceState);
         }
     }
 
@@ -40,6 +59,48 @@ public class DetailActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    private void bindViews(){
+        detailProgressBar=(ProgressBar)findViewById(R.id.detail_progress_bar);
+        detailNoConnectionTextView=(TextView)findViewById(R.id.detail_no_connection_text_view);
+        detailProgressBar.setVisibility(View.INVISIBLE);
+        detailNoConnectionTextView.setVisibility(View.INVISIBLE);
+    }
+
+    private void checkViewsVisibility(Bundle savedInstanceState){
+        if(savedInstanceState.containsKey(IS_DETAIL_NO_CONNECTION_TEXT_VIEW_VISIBLE)){
+            if(savedInstanceState.getBoolean(IS_DETAIL_NO_CONNECTION_TEXT_VIEW_VISIBLE)){
+                detailNoConnectionTextView.setVisibility(View.VISIBLE);
+            }else{
+                detailNoConnectionTextView.setVisibility(View.INVISIBLE);
+            }
+
+            if(savedInstanceState.getBoolean(IS_DETAIL_PROGRESS_BAR_VISIBLE)){
+                detailProgressBar.setVisibility(View.VISIBLE);
+            }else{
+                detailProgressBar.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        detailOnSavedInstance=true;
+        outState.putBoolean(DETAIL_ON_SAVED_INSTANCE,detailOnSavedInstance);
+
+        if(detailProgressBar.getVisibility()==View.VISIBLE){
+            outState.putBoolean(IS_DETAIL_PROGRESS_BAR_VISIBLE,true);
+        }else{
+            outState.putBoolean(IS_DETAIL_PROGRESS_BAR_VISIBLE,false);
+        }
+
+        if(detailNoConnectionTextView.getVisibility()==View.VISIBLE){
+            outState.putBoolean(IS_DETAIL_NO_CONNECTION_TEXT_VIEW_VISIBLE,true);
+        }else{
+            outState.putBoolean(IS_DETAIL_NO_CONNECTION_TEXT_VIEW_VISIBLE,false);
+        }
+
+        super.onSaveInstanceState(outState);
     }
 
     public class SpotDetails{
